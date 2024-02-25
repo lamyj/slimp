0aeb4374df7cae2442bdb466b4b0686fa3a0dd41
Julien Lamy <lamy@unistra.fr>
2024-02-25 15:20:10 +0100

    Optimize r_squared

--- slimp/stats.py
+++ slimp/stats.py
@@ -2,7 +2,7 @@ def r_squared(model, **kwargs):
     # https://avehtari.github.io/bayes_R2/bayes_R2.html
     
     mu, _ = model.predict(model.data, **kwargs)
-    var_mu = mu.std("columns")**2
+    var_mu = mu.var("columns")
     var_sigma = model.draws["sigma"]**2
     
     return var_mu/(var_mu+var_sigma)
