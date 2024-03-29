a68e5206392deb6f141fc766ccba5dad33d2ed0d
Julien Lamy <lamy@unistra.fr>
2024-03-29 09:05:04 +0100

    Add CI setup

---
+++ .ci/build/conda.py
@@ -0,0 +1,9 @@
+import subprocess
+import sys
+
+conda = sys.argv[1] if len(sys.argv) >= 2 else "conda"
+
+subprocess.check_call([
+    conda, "install", "--yes", "-c", "conda-forge",
+    "cmake", "cmdstan", "formulaic", "matplotlib", "ninja", "numpy", "pandas",
+    "pybind11", "seaborn"])
