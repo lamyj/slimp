00b4f585fc44f6a2872b0f65a8569933e3700f88
Julien Lamy <lamy@unistra.fr>
2024-03-29 15:19:17 +0100

    Add build CI

---
+++ .ci/build/build.py
@@ -0,0 +1,28 @@
+import multiprocessing
+import os
+import re
+import subprocess
+import sys
+
+workspace = os.environ["WORKSPACE"]
+build_dir = os.environ.get("BUILD_DIR", os.path.join(workspace, "build"))
+install_dir = os.environ.get("INSTALL_DIR", os.path.join(workspace, "install"))
+
+for dir in [build_dir, install_dir]:
+    if not os.path.isdir(dir):
+        os.makedirs(dir)
+
+subprocess.check_call(
+    [
+        "cmake", "-G", "Ninja", f"-DPython_EXECUTABLE={sys.executable}",
+        "-DCMAKE_BUILD_TYPE=Release", f"-DCMAKE_INSTALL_PREFIX={install_dir}", 
+        workspace],
+    cwd=build_dir,
+    env=os.environ|{
+        "STAN_THREADS":"1", "STAN_NO_RANGE_CHECKS":"1", "CXX":"g++"})
+
+subprocess.check_call(
+    [
+        "cmake", "--build", ".", "--target", "install", "--config", "Release",
+        "--parallel"],
+    cwd=build_dir)
--- .ci/build/conda.py
+++ .ci/build/conda.py
@@ -5,5 +5,5 @@ conda = sys.argv[1] if len(sys.argv) >= 2 else "conda"
 
 subprocess.check_call([
     conda, "install", "--yes", "-c", "conda-forge",
-    "cmake", "cmdstan", "formulaic", "matplotlib", "ninja", "numpy", "pandas",
-    "pybind11", "seaborn"])
+    "cmake", "cmdstan", "eigen", "formulaic", "matplotlib", "ninja", "numpy",
+    "pandas", "pybind11", "seaborn"])
