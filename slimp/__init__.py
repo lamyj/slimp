619b6b602a223617e3f739e2f55464186e0c357f
Julien Lamy <lamy@unistra.fr>
2024-03-30 11:37:43 +0100

    Remove reference to compile module

--- slimp/__init__.py
+++ slimp/__init__.py
@@ -1,5 +1,4 @@
 from ._slimp import action_parameters
-from .compile import compile
 from .model import Model
 from .plots import KDEPlot, parameters_plot, predictive_plot
 from .predictor_mapper import PredictorMapper
