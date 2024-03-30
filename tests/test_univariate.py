3446962bab352ffa93544cbedda0187687f7e124
Julien Lamy <lamy@unistra.fr>
2024-03-30 08:11:31 +0100

    Test additional univariate programs

--- tests/test_univariate.py
+++ tests/test_univariate.py
@@ -58,6 +58,10 @@ class TestUnivariate(unittest.TestCase):
             self._test_sampler_parameters(model)
             self._test_model_diagnostics(model)
             self._test_model_draws(model)
+            self._test_model_log_likelihood(model)
+            self._test_model_prior_predict(model)
+            self._test_model_posterior_epred(model)
+            self._test_model_posterior_predict(model)
     
     def _test_model_data(self, model):
         self.assertEqual(self.formula, model.formula)
@@ -92,31 +96,64 @@ class TestUnivariate(unittest.TestCase):
         self.assertEqual(
             list(model.draws.columns),
             ["Intercept_c", "x", "y", "sigma", "Intercept"])
+        self.assertEqual(len(model.draws), 4000)
         numpy.testing.assert_allclose(
-            model.draws.describe().values,
+            model.draws.iloc[:5,:].values,
             [
-                [4000, 4000, 4000, 4000, 4000],
-                [
-                    2.32920494e+01, 9.07393946e-01, 2.03522925e+00,
-                    1.83309822e+00, 1.00502450e+01],
-                [
-                    1.82854186e-01, 6.24573568e-02, 6.38350738e-02,
-                    1.32399923e-01, 4.51111133e-01],
-                [
-                    2.25925673e+01, 6.84856120e-01, 1.81185481e+00,
-                    1.44426085e+00, 8.46896348e+00],
-                [
-                    2.31733098e+01, 8.66278326e-01, 1.99290926e+00,
-                    1.73854068e+00, 9.75405391e+00],
-                [
-                    2.32906485e+01, 9.07013313e-01, 2.03545372e+00,
-                    1.82685314e+00, 1.00437696e+01],
-                [
-                    2.34117638e+01, 9.48450080e-01, 2.07808404e+00,
-                    1.91598202e+00, 1.03500511e+01],
-                [
-                    2.40042409e+01, 1.11888080e+00, 2.27348756e+00,
-                    2.43737262e+00, 1.17039339e+01]])
+                [23.4592030, 0.94076130, 2.00342470, 1.806797348, 10.21036598],
+                [23.3905170, 0.98711629, 1.91642734, 1.881650569, 10.32457061],
+                [23.4079064, 0.91977690, 2.14917482, 1.783852860, 9.597623651],
+                [23.0649425, 0.98298884, 2.12437401, 1.843825196, 9.081809700],
+                [23.2145337, 0.96373905, 2.10546865, 1.875964407, 9.403099096]])
+    
+    def _test_model_log_likelihood(self, model):
+        self.assertEqual(
+            model.log_likelihood.shape, (4000, len(self.data)))
+        numpy.testing.assert_allclose(
+            model.log_likelihood.iloc[:5,:5].values,
+            [
+                [-1.6044112, -1.5385061, -1.7323319, -2.9013034, -1.5403785],
+                [-1.4785300, -1.5549794, -1.6483398, -3.0005365, -1.4751144],
+                [-1.5577002, -1.6164186, -1.6576977, -2.6633665, -1.5912700],
+                [-1.6905174, -1.5387873, -1.8659833, -3.1639919, -1.5364730],
+                [-1.6142649, -1.5999491, -1.6913405, -2.6269443, -1.6287000]])
+    
+    def _test_model_prior_predict(self, model):
+        self.assertEqual(
+            model.prior_predict.shape, (4000, len(self.data)))
+        numpy.testing.assert_allclose(
+            model.prior_predict.iloc[:5,:5].values,
+            [
+                [-18.0628612,-12.585432 , -0.2146551, -8.3131347,  1.1200769],
+                [ 78.955988 , 75.438115 , 71.8809921, 68.3484672, 64.6451405],
+                [ 78.2182381, 77.1482612, 49.8129586, 47.0666833, 23.8685327],
+                [-75.2312012,-46.1937697,-18.170517 , 10.4951465, 37.1835187],
+                [ 13.7191093, 13.491873 ,  1.2439165,  4.5655172, -3.7208326]])
+    
+    def _test_model_posterior_epred(self, model):
+        self.assertEqual(
+            model.posterior_epred.shape, (4000, len(self.data)))
+        numpy.testing.assert_allclose(
+            model.posterior_epred.iloc[:5,:5].values,
+            [
+                [10.210366 ,11.1511273,12.0918886,13.0326499,13.9734112],
+                [10.6838411,11.4645385,12.2452359,13.0259334,13.8066308],
+                [10.5832679,11.4779999,12.3727319,13.2674639,14.1621959],
+                [ 9.9617344,10.8754661,11.7891979,12.7029297,13.6166614],
+                [10.3245706,11.3116869,12.2988032,13.2859195,14.2730358]])
+    
+    def _test_model_posterior_predict(self, model):
+        self.assertEqual(
+            model.posterior_predict.shape, (4000, len(self.data)))
+        numpy.testing.assert_allclose(
+            model.posterior_predict.iloc[:5,:5].values,
+            [
+                [10.0789502,13.0932424,13.2128267,14.2029392,16.1124219],
+                [12.4243714,13.452148 ,12.1742729,13.2278094,13.6020354],
+                [14.0521864,11.4358118,12.9317417,14.3156692,14.6669161],
+                [11.2820318,12.7343338,10.5181746,12.7999644,13.3685442],
+                [ 8.4333726,10.277509 ,10.1294386,13.1056473,12.9209938]]
+)
 
 if __name__ == "__main__":
     unittest.main()
