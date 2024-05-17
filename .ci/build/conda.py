384c41da887bd337095ce5a2bc1a4ffb6cafef88
Julien Lamy <lamy@unistra.fr>
2024-05-17 16:03:15 +0200

    Improve robustness w.r.t sundials version

--- .ci/build/conda.py
+++ .ci/build/conda.py
@@ -6,4 +6,4 @@ conda = sys.argv[1] if len(sys.argv) >= 2 else "conda"
 subprocess.check_call([
     conda, "install", "--yes", "-c", "conda-forge",
     "boost", "cmake", "cmdstan", "eigen", "formulaic", "matplotlib", "ninja",
-    "numpy", "pandas", "pybind11", "seaborn", "sundials", "tbb"])
+    "numpy", "pandas", "pybind11", "seaborn", "sundials<7", "tbb"])
