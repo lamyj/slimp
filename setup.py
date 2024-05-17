45bb92d6590bf21b8ec39141dff1093c361b8dac
Julien Lamy <lamy@unistra.fr>
2024-05-17 17:25:35 +0200

    Bump version: 0.4.0 → 0.5.0

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ setuptools.command.build.build.sub_commands.append(("build_cmake", None))
 long_description = open(os.path.join(here, "README.md")).read()
 setuptools.setup(
     name="slimp",
-    version="0.4.0",
+    version="0.5.0",
     
     description="Linear models with Stan and Pandas",
     long_description=long_description,
