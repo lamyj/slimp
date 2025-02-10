bab052f50b4b77866090ea79eb9304a2cc50ae58
Julien Lamy <lamy@unistra.fr>
2025-02-10 17:35:21 +0100

    Update build dependencies

--- .ci/build/conda.py
+++ .ci/build/conda.py
@@ -6,4 +6,5 @@ conda = sys.argv[1] if len(sys.argv) >= 2 else "conda"
 subprocess.check_call([
     conda, "install", "--yes", "-c", "conda-forge",
     "boost", "cmake", "cmdstan", "eigen", "formulaic", "matplotlib", "ninja",
-    "numpy", "pandas", "pybind11", "seaborn", "sundials<7", "tbb"])
+    "numpy", "pandas", "pybind11", "r-lme4", "r-systemfit", "seaborn",
+    "sundials<7", "tbb", "xarray", "xtensor", "xtensor-python"])
