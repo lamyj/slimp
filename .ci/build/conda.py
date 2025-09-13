a409a87363141e51aa119d5fba962f0a410e6ab7
Julien Lamy <lamy@unistra.fr>
2025-09-13 11:07:36 +0200

    Fix conda dependencies

--- .ci/build/conda.py
+++ .ci/build/conda.py
@@ -5,6 +5,6 @@ conda = sys.argv[1] if len(sys.argv) >= 2 else "conda"
 
 subprocess.check_call([
     conda, "install", "--yes", "-c", "conda-forge",
-    "boost", "cmake", "cmdstan", "eigen", "formulaic", "matplotlib", "ninja",
-    "numpy", "pandas", "pybind11", "r-lme4", "r-systemfit", "rpy2", "seaborn",
-    "sundials<7", "tbb", "xarray", "xtensor", "xtensor-python"])
+    "arviz", "boost", "cmake", "cmdstan", "eigen", "formulaic", "matplotlib",
+    "ninja", "numpy", "pandas", "pybind11", "r-lme4", "r-systemfit", "rpy2",
+    "seaborn", "tbb", "xarray", "xtensor", "xtensor-python"])
